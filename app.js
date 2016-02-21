48bd5eb9b4f4c3814fb39c74e05df1b494a47abc
Luke Igel <luke.igel@gmail.com>
2016-02-20 19:23:50 -0600

    pls work

--- app.js
+++ app.js
@@ -1,19 +1,55 @@
-var express = require('express'),
-	cors = require('cors'),
-	bodyParser = require('body-parser'),
-	app = express.createServer(express.logger()),
-	io = require('socket.io').listen(app),
-	routes = require('./routes');
+//dependencies
+ var express = require('express');
+ var cors = require('cors');
+ var bodyParser = require('body-parser');
+ 
+ var app = express();
+ app.use(bodyParser.json());
+ app.use(cors());
+ 
+ //message object
+ var message = {
+ 	timestamp: new Date(),
+ 	message: "Test message",
+ 	username: "Luke"
+ }
+ 
+ var messages = [message]; //make an array to hold messages
+ 
+ app.get('/messages', function(req, res) {  //req = request, res = response
+ 	res.json(messages);
+ });
+ 
+ app.get('/messages/:id', function(req,res) {
+ 	var message = messages[req.params.id];
+ 	res.json(message);
+ });
+ 
+ app.post('/messages', function(req,res){
+ 	var newMessage = {
+ 		message:req.body.message,
+ 		username:req.body.username,
+ 		timestamp: new Date()
+ 	};
+ 
+ 	messages.push(newMessage);
+ 	res.json(messages);
+ });
+ 
+ 
+ app.listen(process.env.PORT || 4730); //if port doesn't work, use port 4730
 
-//configuration
 
-app.use(bodyParser.json());
-app.use(cors());
-
-io.configure(function () { 
+//express = require('express'),
+/*io.configure(function () { 
   io.set("transports", ["xhr-polling"]); 
   io.set("polling duration", 10); 
-});
+});*/
+	//app = module.exports.app = express();
 
-app.listen(process.env.PORT || 5000);
-
+//var server = http.createServer(app);
+//var io = require('socket.io').listen(server);  //pass a http.Server instance
+	//server.listen(80);  //listen on port 80
+//routes = require('./routes'),
+//server.listen(4730);
+//var http = require('http');
