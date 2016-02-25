bf48be360450334b88caeef61c37f27228813f54
Luke Igel <luke.igel@gmail.com>
2016-02-24 18:20:01 -0600

    changes

--- app.js
+++ app.js
@@ -13,6 +13,29 @@
  app.use(bodyParser.json());
  app.use(cors());
  
+ var messages = []; //make an array to hold messages
+
+app.get('/messages', function(req, res) {  //req = request, res = response
+	res.json(messages);
+});
+
+app.get('/messages/:id', function(req,res) {
+	var message = messages[req.params.id];
+	res.json(message);
+});
+
+app.post('/messages', function(req,res){
+	var newMessage = {
+		message:req.body.message,
+		username:req.body.username,
+		timestamp: new Date();
+	};
+
+	messages.push(newMessage);
+	res.json(messages);
+});
+
+
 /*io.configure(function () { 
   io.set("transports", ["xhr-polling"]); 
   io.set("polling duration", 10); 
