2b682217fdaf7f12aca42001528d52ce6462d956
Luke Igel <luke.igel@gmail.com>
2016-03-06 17:41:00 -0600

    reverts back

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-/*
 //dependencies
 var express = require('express');
 var cors = require('cors');
@@ -31,7 +30,7 @@ app.post('/users', function(req, res){
 });*/
 
 
-/*
+
 app.get('/rooms', function(req, res) {
 	res.json(rooms);
 });
@@ -53,7 +52,7 @@ app.post('/rooms', function(req, res) {
 
 
 
-app.get('/rooms/:id/messages', function(req, res) {  //req = request, res = response
+app.get('/messages', function(req, res) {  //req = request, res = response
 	res.json(messages);
 });
 
@@ -73,59 +72,6 @@ app.post('/messages', function(req,res){
 	res.json(messages);
 });
 
-
-
-app.listen(process.env.PORT || 4730); //if port doesn't work, use port 4730
-*/
-
-//dependencies
-var express = require('express');
-var cors = require('cors');
-var bodyParser = require('body-parser');
-
-var app = express();
-app.use(bodyParser.json());
-app.use(cors());
-
-var messages = []; //make an array to hold messages
-
-app.get('/messages', function(req, res) {  //req = request, res = response
-	res.json(messages);
-});
-
-app.get('/messages/:id', function(req,res) {
-	var message = messages[req.params.id];
-	res.json(message);
-});
-
-function getDistance(lat1, lat2, lon1, lon2){
-	var R = 6371000; //Earth's radius in metres
-	var lat1Rad = lat1.toRadians();
-	var lat2Rad = lat2.toRadians();
-	var deltaLat = (lat2-lat1).toRadians();
-	var deltaLon = (lon2-lon1).toRadians();
-
-	var a = Math.sin(deltaLat/2) * Math.sin(deltaLat/2) +
-	Math.cos(lat1Rad) * Math.cos(lat2Rad) *
-	Math.sin(deltaLon/2) * Math.sin(deltaLon/2);
-var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));  //angular distance in radians
-
-var d = R * c;	//distance between the two points (in meters?)
-}
-
-app.post('/messages', function(req,res){
-	var newMessage = {
-		message:req.body.message,
-		username:req.body.username,
-		timestamp: new Date(),
-		lat:req.body.lat,
-		lon:req.body.lon,
-		distance:req.body
-	};
-
-	messages.push(newMessage);
-	res.json(messages);
-});
 
 
 app.listen(process.env.PORT || 4730); //if port doesn't work, use port 4730
