826c2ef841691796c9de79aae714dd256c291359
Luke Igel <luke.igel@gmail.com>
2016-04-14 20:51:57 -0500

    changes CORS because reasons?

--- app.js
+++ app.js
@@ -99,12 +99,12 @@ io.sockets.on('connection', function(socket){
 });
 
 
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", 'POST,GET,DELETE,PUT,OPTIONS');
   next();
-});
+});*/
 
 
 // Configure CORS (Cross-Origin Resource Sharing) Headers 
